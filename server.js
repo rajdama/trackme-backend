0dea03108f7f6392e3819c4b0df99e2e8b5c3666
unknown <Raj Dama>
2023-06-14 20:40:11 +0530

    adding cors

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express()
 const cors = require('cors')
 
 env.config()
-app.use(cors())
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+)
 app.use(express.json())
 const port = process.env.PORT || 3000
 
