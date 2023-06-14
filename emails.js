c1e2f048aebaf67f41fed72465e1e1bbad8e40f1
unknown <Raj Dama>
2023-06-14 21:08:22 +0530

    cron

--- emails.js
+++ emails.js
@@ -21,7 +21,25 @@ exports.sendEmails = async () => {
   const userList = await users.list()
   const usersDetails = userList.users
 
-  schedule.scheduleJob({ hour: 21, minute: 5 }, function () {
+  schedule.scheduleJob({ hour: 21, minute: 10 }, function () {
+    usersDetails.forEach((item) => {
+      let mailBreakfastDetails = {
+        from: 'rajdama1729@gmail.com',
+        to: `${item.email}`,
+        subject: 'Time For Dinner',
+        text: 'Hey Pal! I its time you have your breakfast if you have not done yet, if you have already had comeon track it and continue your progress',
+      }
+      mailTransporter.sendMail(mailBreakfastDetails, function (err, data) {
+        if (err) {
+          console.log('Error Occurs')
+        } else {
+          console.log('Email sent successfully')
+        }
+      })
+    })
+  })
+
+  schedule.scheduleJob({ hour: 21, minute: 12 }, function () {
     usersDetails.forEach((item) => {
       let mailBreakfastDetails = {
         from: 'rajdama1729@gmail.com',
