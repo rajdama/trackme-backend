1c85981674010a938642e288a080bd2d23ddb7a1
unknown <Raj Dama>
2023-06-21 18:02:55 +0530

    chatbot update

--- controller/chatBot.js
+++ controller/chatBot.js
@@ -2,10 +2,11 @@ const axios = require('axios')
 
 exports.chatBot = async (req, res) => {
   try {
+    let content = `Is the statement "${req.body.msg}" related to health or food or excercise or fitness. Answer me only true or false in lowercase`
     response = await axios.post(
       'https://chatgpt53.p.rapidapi.com/',
       {
-        messages: [{ role: 'user', content: `${req.body.msg}` }],
+        messages: [{ role: 'user', content }],
         temperature: 1,
       },
       {
@@ -16,8 +17,34 @@ exports.chatBot = async (req, res) => {
         },
       }
     )
-    res.status(200).send(response.data.choices)
+
+    let message = ''
+    console.log(response.data.choices[0].message.content == 'false')
+    if (response.data.choices[0].message.content == 'false') {
+      message =
+        'As an AI health assistant, I can help you only with health related topics, you are free to ask me anything and everything related to health and fitness '
+      res.status(200).send(message)
+    } else {
+      message = req.body.msg
+
+      let botResponse = await axios.post(
+        'https://chatgpt53.p.rapidapi.com/',
+        {
+          messages: [{ role: 'user', content: message }],
+          temperature: 1,
+        },
+        {
+          headers: {
+            'content-type': 'application/json',
+            'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
+            'X-RapidAPI-Host': 'chatgpt53.p.rapidapi.com',
+          },
+        }
+      )
+      res.status(200).send(botResponse.data.choices[0].message.content)
+    }
   } catch (error) {
+    console.log(error)
     res.status(500).send(error || 'Something went wrong')
   }
 }
